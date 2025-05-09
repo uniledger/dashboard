8def5fa2953376a3e45c167a57ee6d9291b1be28
chris-ashworth <chris.ashworth@ledgerrocket.com>
2025-05-09 14:51:11 +0100

    Add missing waitFor dependency

--- src/components/accounts/AccountList.test.js
+++ src/components/accounts/AccountList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import AccountList from './AccountList';
 import * as useAccountsModule from '../../hooks/useAccounts';
 import { MemoryRouter } from 'react-router-dom';
